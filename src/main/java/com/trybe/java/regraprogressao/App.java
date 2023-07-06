14f70ee12e32440eea240b38ce7592493ae70021
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-07-06 10:19:29 -0300

    :sparkles: [feat] nota-da-atividade

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -17,12 +17,15 @@ public class App {
     String qty = scanner.next();
     int qtyInt = Integer.parseInt(qty);
 
-    for(int i = 1; i < qtyInt; i += 1) {
+    for(int i = 1; i < qtyInt + 1; i += 1) {
       System.out.println("Digite o nome da atividade " + i);
       String atv = scanner.next();
       System.out.println("Digite o peso da atividade " + i);
       String peso = scanner.next();
       int pesoInt = Integer.parseInt(peso);
+      System.out.println("Digite a nota obtida para " + atv);
+      String nota = scanner.next();
+      int notaInt = Integer.parseInt(nota);
 
     }
 
