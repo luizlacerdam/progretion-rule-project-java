02fadcd3bf74b5f09ee3a74583cf8e0b5cc07f14
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-07-06 09:53:02 -0300

    :sparkles: [feat] estrutura-inicial

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,15 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    String qty = scanner.next();
+    int qtyInt = Integer.parseInt(qty);
+
+    for(int i = 0; i < qtyInt; i += 1) {
+
+    }
+
   }
 }
